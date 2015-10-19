723dd7f1b0689826ab0e91d70146b198185cf74a
ekashivagui <mushira4@gmail.com>
2015-10-19 00:20:58 -0200

    Correcting shopping cart

--- src/main/java/br/com/spring/mvc/basics/controller/ShoppingCartController.java
+++ src/main/java/br/com/spring/mvc/basics/controller/ShoppingCartController.java
@@ -19,28 +19,30 @@ public class ShoppingCartController {
 
 	@Autowired
 	private ProductDAO productDAO;
-	
+
 	@Autowired
 	private ShoppingCart shoppingCart;
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView add(Integer productId, @RequestParam BookType bookType){
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView add(Integer productId, 
+	/** Indicates that a method parameter should be bound to a web request parameter **/
+	@RequestParam BookType bookType) {
 		Product product = productDAO.find(productId);
 		ShoppingItem item = new ShoppingItem(product, bookType);
 		shoppingCart.add(item);
-		
+
 		ModelAndView modelAndView = new ModelAndView("redirect:/shopping");
 		modelAndView.addObject("shoppingCart", shoppingCart);
 		return modelAndView;
 	}
-	
-	@RequestMapping(value="remove",method=RequestMethod.POST)
-	public String remove(Long productId, BookType bookType){
+
+	@RequestMapping(value = "remove", method = RequestMethod.POST)
+	public String remove(Long productId, BookType bookType) {
 		return "";
 	}
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public ModelAndView items(){
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView items() {
 		ModelAndView modelAndView = new ModelAndView("shoppingCart/items");
 		modelAndView.addObject("shoppingCart", shoppingCart);
 		return modelAndView;
